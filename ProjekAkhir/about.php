4efe9837b773821857e2bdeb8280784c598f2e28
Muchlas Andrey <116377289+Leomuch1508@users.noreply.github.com>
2023-11-16 22:43:38 +0800

    Update about.php

--- ProjekAkhir/about.php
+++ ProjekAkhir/about.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Limude's Ice Cream</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -13,29 +13,17 @@
     <section class="about">
         <h1>About Us</h1><hr>
         <div class="about-content">
-            <img src="gambar/bg5.png" alt="" id="gambar">
-            <table class="biodata">
-                <tr>
-                    <td>Nama Website</td>
-                    <td>:</td>
-                    <td>Limude's ice Cream</td>
-                </tr>
-                <tr>
-                    <td>Tempat Tanggal Lahir</td>
-                    <td>:</td>
-                    <td>Samarinda, 18 November 2004</td>
-                </tr>
-                <tr>
-                    <td>Email</td>
-                    <td>:</td>
-                    <td>limude'sicecream@gmail.com</td>
-                </tr>
-                <tr>
-                    <td>Instagram</td>
-                    <td>:</td>
-                    <td>@limude'sIceCream</td>
-                </tr>
-            </table>
+            <img src="gambar/bg1.png" alt="" id="gambar">
+            <div class="biodata">
+                <p>
+                Selamat datang di Limude's Ice Cream! Kami adalah tempat di mana kelezatan bertemu dengan kreasi tak terbatas dari es krim. Sejak [tahun pendirian], kami telah menjadi bagian tak terpisahkan dari komunitas pencinta es krim di seluruh [lokasi].
+                Kami percaya bahwa es krim bukan sekadar makanan penutup, melainkan karya seni yang bisa menyatukan orang dalam kenikmatan yang menyegarkan. Setiap gigitan es krim kami adalah perpaduan hati-hati antara bahan-bahan berkualitas tinggi dan imajinasi tak terbatas.
+                Di Limude's Ice Cream, kami berkomitmen untuk menyajikan produk berkualitas terbaik. Kami memilih bahan-bahan segar dan berkualitas tinggi untuk menciptakan berbagai rasa yang menggugah selera, mulai dari klasik yang lezat hingga inovasi yang menggembirakan.
+                Tapi lebih dari sekadar es krim, kami adalah tentang pengalaman. Kami ingin setiap kunjungan ke tempat kami menjadi momen istimewa bagi Anda dan keluarga. Suasana ramah dan pelayanan yang hangat adalah bagian dari komitmen kami untuk menciptakan kenangan yang tak terlupakan.
+                Kami bangga menjadi bagian dari komunitas lokal kami. Selain menyajikan es krim terbaik, kami juga berusaha untuk mendukung inisiatif lingkungan dan kemanusiaan di sekitar kami.
+                Terima kasih telah menjadi bagian dari perjalanan kami di dunia es krim. Kami berharap dapat terus memanjakan lidah Anda dengan kreasi kami yang tak terlupakan.
+                </p>
+            </div>
         </div>
     </section>
 
